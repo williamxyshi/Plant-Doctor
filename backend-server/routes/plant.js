fd6c5294a5f44b5836a4957f00011d66bb902cd3
williamxyshi <williamxyshi@gmail.com>
2020-06-16 17:41:54 -0400

    init node/express/mongo

---
+++ backend-server/routes/plant.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const { check, validationResult} = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+
+const auth = require("./../middleware/auth");
+
+const User = require("../models/user_model");
+
+
+router.get("/planttest", async (req, res) => {
+    try {
+
+
+        res.send({ message: "plant test" });
+
+    } catch (e) {
+      res.send({ message: "Error in Fetching user" });
+    }
+  });
+
+module.exports = router;
